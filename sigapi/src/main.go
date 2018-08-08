44c49929357127978434df4ba0f07f6f9a1256b3
Denys Vasyliev [GL] <Denys.Vasyliev@qsc.com>
2018-08-08 09:02:19 +0300

    fix rmq

--- sigapi/src/main.go
+++ sigapi/src/main.go
@@ -248,6 +248,6 @@ func API(verson string) apiStruct {
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
+		log.Print(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
--- spool/src/main.go
+++ spool/src/main.go
@@ -95,8 +95,6 @@ func main() {
 
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
-
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
@@ -104,7 +102,6 @@ func main() {
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/version", versionHandler)
